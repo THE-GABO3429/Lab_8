63890c6072e7bbb3d6140eec056e70347ee4e012
Gabriel R. <0358929@gmail.com>
2025-03-27 09:53:38 -0400

    just did smome testing

--- CommentTest.java
+++ CommentTest.java
@@ -39,4 +39,27 @@ public class CommentTest
     public void tearDown()
     {
     }
+
+    @Test
+    public void testingAuthorAndRatingDetails()
+    {
+        SalesItem salesIte1 = new SalesItem("Cheese", 1000000);
+        assertEquals(true, salesIte1.addComment("IHateCheese", "Why do they make these, it's awful!", 1));
+        salesIte1.showInfo();
+    }
+
+    @Test
+    public void testingUpVote()
+    {
+        SalesItem salesIte1 = new SalesItem("Cheese", 1000000);
+        assertEquals(true, salesIte1.addComment("Dave", "The best cheese ever, worth the price, sue me!", 5));
+        salesIte1.downvoteComment(0);
+        salesIte1.showInfo();
+        salesIte1.upvoteComment(0);
+        salesIte1.showInfo();
+        salesIte1.upvoteComment(0);
+        salesIte1.showInfo();
+    }
 }
+
+
